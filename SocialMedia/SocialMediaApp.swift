4de46430a5d89ed30a1011e3dfce666c3f633e9c
Jack <finnis.j@etoncollege.org.uk>
2021-05-19 13:19:46 +0100

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by Finnis on 19/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
